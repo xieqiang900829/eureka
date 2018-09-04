ff61e1244fbb14fbb40a3f6aaa90be6e0f8ac39c
xieqiang <xieqiang@wdai.com>
2018-09-04 16:51:54 +0800

    更新application类名称

--- src/main/java/com/example/demo/Application.java
+++ src/main/java/com/example/demo/Application.java
@@ -7,9 +7,9 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer
 @SpringBootApplication
-public class SpringbootdemoApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringbootdemoApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 }
